fb21e049405a0bb7c892ea878851f08d63c4dd26
006458 <igor.rudel@realizecfi.com.br>
2024-08-28 12:28:40 -0300

    Commit inicial

---
+++ api-one/src/test/java/br/xksoberbado/apione/ApiOneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.xksoberbado.apione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
